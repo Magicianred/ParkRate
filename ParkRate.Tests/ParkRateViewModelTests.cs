ebe7ede8aa10c49b897e4035cbf6f58ac7e8c1c8
Luca Piccinelli <luca.picci@gmail.com>
2019-09-26 07:46:23 +0200

    viemodel logic

---
+++ ParkRate.Tests/ParkRateViewModelTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ParkRate.ViewModel;
+
+namespace ParkRate.Tests
+{
+    public class ParkRateViewModelTests
+    {
+        [Test]
+        public void OutTime_Defaults_ToNow()
+        {
+            ParkRateViewModel viewModel = new ParkRateViewModel();
+            Assert.That(viewModel.OutTime, Is.EqualTo(DateTime.Now).Within(1).Seconds);
+        }
+
+        [Test]
+        public void Given_HourOfArrival_IExpect_ARateAmount()
+        {
+            int day = 26;
+            int month = 9;
+            int year = 2019;
+            int hour = 12;
+            int minutes = 15;
+            ParkRateViewModel viewModel = new ParkRateViewModel
+            {
+                ArrivalDate = $"{day}{month}{year}",
+                ArrivalTime = $"{hour}{minutes}",
+                OutTime = new DateTime(year, month, day, hour + 1, minutes, 0)
+            };
+
+            decimal expectedRate = 0;
+            Assert.AreEqual(expectedRate, viewModel.RateValue);
+        }
+
+    }
+}
---
+++ ParkRate/ViewModel/ParkRateViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ParkRate.Annotations;
+
+namespace ParkRate.ViewModel
+{
+    public class ParkRateViewModel : INotifyPropertyChanged
+    {
+        private string _arrivalTime;
+        private string _arrivalDate;
+        private decimal _rateValue;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ParkRateViewModel()
+        {
+            OutTime = DateTime.Now;
+        }
+
+        public String ArrivalTime
+        {
+            get => _arrivalTime;
+            set
+            {
+                _arrivalTime = value;
+                OnPropertyChanged(nameof(ArrivalTime));
+                RateValue = 0;
+            }
+        }
+
+        public String ArrivalDate
+        {
+            get => _arrivalDate;
+            set
+            {
+                _arrivalDate = value;
+                OnPropertyChanged(nameof(ArrivalDate));
+                RateValue = 0;
+            }
+        }
+
+        public Decimal RateValue
+        {
+            get => _rateValue;
+            private set
+            {
+                _rateValue = value;
+                OnPropertyChanged(nameof(RateValue));
+            }
+        }
+
+        public DateTime OutTime { get; set; }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
